d2de44f1b0edfabbea4f0f92c1a56559e25b37cd
ihlas_sul <ihlassmada@gmail.com>
2021-11-03 10:31:04 +0800

    tugas 24 javascript
    done

---
+++ tugas24.js
@@ -0,0 +1,4 @@
+let array=['32','33','16','40','9','7','11','2','44','1','66' ]
+console.log('sebelumnya : '+array)
+console.log('Ascending : '+array.sort(points.sort(function(a, b){return a-b})))
+console.log('Descending : '+array.sort(function(a, b){return b-a}))
